b70e33eb5a5b7718fcba6e8b9db7407258aafde5
Steffen <steffaeaeaen@gmail.com>
2025-05-25 14:14:02 +0200

    Cleanup build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("java")
     application
     kotlin("jvm") version "2.1.20"
 }
@@ -18,22 +17,19 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jspecify:jspecify:1.0.0")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation(kotlin("stdlib-jdk8"))
 }
 
-java {
-    toolchain.languageVersion = JavaLanguageVersion.of(23)
+kotlin {
+    jvmToolchain(23)
 }
 
 tasks.test {
     useJUnitPlatform()
 }
-kotlin {
-    jvmToolchain(23)
-}
+
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.compilerOptions {
     freeCompilerArgs.set(listOf("-Xwhen-guards", "-Xcontext-parameters"))
